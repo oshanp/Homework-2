d7b5e687e8821efe3f4efb37ab4df5e4afec34f2
oshanp <81204395+oshanp@users.noreply.github.com>
2021-03-23 19:25:17 -0600

    Update matric_dynamic.h

--- matric_dynamic.h
+++ matric_dynamic.h
@@ -19,14 +19,14 @@ matrix create_initval(int rdim, int cdim, T val);
 matrix create_initvals(int rdim, int cdim, T* initval);
 void destroy(matrix);
 void matrix_print(matrix);
-T retrieve(int row, int col, matrix m);  /* retrieve an element from m */
-void assign(int row, int col, matrix*, T val); /* assign a value to an element of m */
-void equate(matrix* m1, matrix* m2);  /* m1 = m2 */
+T retrieve(int row, int col, matrix m); 
+void assign(int row, int col, matrix*, T val); 
+void equate(matrix* m1, matrix* m2);  
 matrix add(matrix, matrix);
 matrix subtract(matrix, matrix);
 matrix negate(matrix);
 matrix multiply(matrix, matrix);
 matrix scalar_multiply(T scalar, matrix);
-/* remaining function prototypes not shown */
+
 
 #endif
