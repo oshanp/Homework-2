cd18a96068239d5f35e981740382eff68f2a14ad
oshanp <81204395+oshanp@users.noreply.github.com>
2021-03-23 19:10:53 -0600

    Add files via upload

---
+++ main_d.cpp
@@ -0,0 +1,28 @@
+#include "iostream"
+#include "matrix_dynamic.h"
+
+int main() 
+{
+int r,c,i,j;
+double val;
+matrix test, trans;
+
+std::cout << "how many rows?";
+std::cin << r;
+std::cout << "how many colomn?";
+std::cin << c;
+test =create_initval(r,c 0);
+std::cout << std::end1<< "enter elemts of matrix: " << std::end1;
+for(i=0; i<r; ++i)
+for(j=0; i<c; ++j)
+{
+  std::cout <<"enter element for " << " row [" << i+1 ']' << "col [" << j+1 << ']' << " :";
+  std::cin>>val;
+  assign(i,j, &test, val);
+  }
+}
+std::cout << "\n\tYour matrix";
+matrix print(test);
+std::cout << "your matrix transpose";
+matrix transpose(test);
+matrix print(test);
